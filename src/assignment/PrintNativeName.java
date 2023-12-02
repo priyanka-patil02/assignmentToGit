bcbf6203f9368c253d130b5042e3a08c36e330b3
Priya209 <priyankapatil090720@gmail.com>
2023-12-02 20:36:57 +0530

    native name code added

---
+++ src/assignment/PrintNativeName.java
@@ -0,0 +1,25 @@
+package assignment;
+
+public class PrintNativeName
+{
+
+	public static void main(String[] args) 
+	{
+		char c1='\u092A';
+		char c2='\u094D';
+		char c3 ='\u0930';                  //A8EE';//char c12='\u093C';094D
+		char c4='\u093F';
+		char c5='\u093C';
+		char c6='\u092F';
+		char c8='\u0902';
+		char c7='\u093E';
+
+		char c9='\u0915';
+		char c10='\u093E';
+
+
+		System.out.println(c1+""+c2+""+c3+""+c4+""+c5+""+c6+""+c7+""+c8+""+c9+""+ c10);
+
+	}
+
+}

82d6936091f59f104c58a3f5ceafb592ff1943a0
Priya209 <priyankapatil090720@gmail.com>
2023-12-12 01:36:10 +0530

    cade added.

---
+++ src/assignment/Comment_Class.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class Comment_Class 
+{
+	/**
+	 * This method is learn documentation comment in java. <BR/> 
+	 * This line i am writing for HTML presentation
+	 * @param i
+	 * @param j
+	 */
+	 static void commentMethod(int i , int j)
+	 {
+		  
+	  }
+}
---
+++ src/assignment/Main.java
@@ -0,0 +1,13 @@
+package assignment;
+
+public class Main
+{
+
+	public static void main(String[] args) 
+	{
+		//ststic method calling and comment is visible while calling in eclipse
+		Comment_Class.commentMethod( 0,0);
+
+	}
+
+}

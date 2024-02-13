d62ac24184f1f303fdfab0b2485923c79ecfdeef
Priya209 <priyankapatil090720@gmail.com>
2024-02-13 20:46:42 +0530

    pattern1 commit

--- src/starPrograms/Pattern_1.java
+++ src/starPrograms/Pattern_1.java
@@ -6,6 +6,7 @@ public class Pattern_1
 
 	public static void main(String[] args) 
 	{
+		//**************** Pattern 1  *******************//
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -26,7 +27,117 @@ public class Pattern_1
 			
 		}
 		
+	
 		
+		//**************** Pattern 2  *******************//
+		/*
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter row number : ");
+		int row = sc.nextInt();
+		
+		for(int i=1;i<=row;i++)
+		{
+			
+			for (int j=1*(row-i); j>=1; j--)  
+			{
+				
+				System.out.print(" ");
+				
+			}
+			
+			for (int j=1; j<=i; j++ )   
+			{
+				
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+		
+		*/
+		
+		//**************** Pattern 3  *******************//
+		/*
+		 Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter row number : ");
+		int row = sc.nextInt();
+		
+		for(int i=1;i<=row;i++)
+		{
+			for(int j=row;j>=i;j--)
+			{
+				
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+
+		 */
+		
+		//**************** Pattern 4  *******************//
+		
+		/*
+		 Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter row number : ");
+		int row = sc.nextInt();
+		
+		for(int i=1;i<=row;i++)
+		{
+			for (int j=i; j<=row; j++ )   
+			{
+				
+				System.out.print("*");
+				
+			}
+			System.out.println();
+			for (int j=0; j<i; j++)  
+			{
+				
+				System.out.print(" ");
+				
+			}
+			
+			
+		}
+		 */
+		
+		
+		
+		//**************** Pattern 5  *******************//
+		
+		/*
+		 Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter row number : ");
+		int row = sc.nextInt();
+		//int m=0;
+		 int i,j;
+		for( i=0;i<row;i++)
+		{
+			
+			
+			for (j=row-i; j>1; j--)   
+			{
+				
+				System.out.print(" ");
+				
+			}
+			//System.out.println();
+			for (j=0; j<=i; j++)  
+			{
+				
+				System.out.print("* ");    //give space after *
+				
+				
+			}
+			
+			System.out.println();
+		}
+		*/
 	}
 
 }
